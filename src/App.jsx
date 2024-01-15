ebd765dbee916ad5aedf7cd1aa744089bb83c98c
aimardev <amiarnino.io@gmail.com>
2024-01-15 17:41:54 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useEffect, useMemo, useRef } from 'react'
 import NarrativeMessage from './components/NarrativeMessage'
 import ColbyChart from './components/ColbyChart'
 import ColbyChartForm from './components/ColbyChartForm'
+import ColbyLoader from './components/ColbyLoader'
 import { ChartProvider } from './context/ChartContext'
+import { useIsLoading } from './hooks/useIsLoading'
 
 import './App.scss'
 
 function App() {
+  const isLoading = useIsLoading()
+
   return (
-    <ChartProvider>
+    isLoading ? <ColbyLoader /> : <ChartProvider>
       <div className="max-w-7xl mx-auto">
         <div className="w-full flex ">
           <div className="w-full xl:w-8/12 px-2">

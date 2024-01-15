8d9b19c1e0ec96090908a5325f462cd75f6f8d59
aimardev <amiarnino.io@gmail.com>
2024-01-15 15:43:36 +0000

    updated

--- src/components/ColbyChart.jsx
+++ src/components/ColbyChart.jsx
@@ -7,7 +7,11 @@ import {
     Title,
     Tooltip,
     Legend,
+    LineController, LineElement, PointElement,
+    BarController, 
+    registerables
 } from 'chart.js';
+
 import { Chart as ReactChart } from 'react-chartjs-2';
 import { useChartContext } from '../hooks/useChartContext'
 import ChartDataLabels from 'chartjs-plugin-datalabels';
@@ -17,11 +21,17 @@ ChartJS.register(
     CategoryScale,
     LinearScale,
     BarElement,
+    BarController,
+    LineController,
+    LineElement,
+
+    PointElement,
     Title,
     Tooltip,
     Legend,
     ChartDataLabels,
     AnnotationPlugin,
+    ...registerables
 );
 
 
@@ -29,7 +39,8 @@ ChartJS.register(
 const ColbyChart = () => {
     const context = useChartContext()
     const { state: { options, data, chartType }, chartRef, draggerPlugin } = context
-    
+
+
     return (
         <ReactChart ref={chartRef} type={chartType} options={options} data={{ ...data }}
             plugins={[draggerPlugin]}

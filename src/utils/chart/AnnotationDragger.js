042a080568fff1c724b28e90e11e56f5340b2d5b
aimardev <amiarnino.io@gmail.com>
2024-01-19 15:02:28 +0000

    update thte dragger

--- src/utils/chart/AnnotationDragger.js
+++ src/utils/chart/AnnotationDragger.js
@@ -12,9 +12,115 @@ export class AnnotationDragger {
     }
     beforeEvent(chart, args, options) {
         if (this.handleDrag(args.event)) {
-            args.changed = true;
+            // args.changed = true;
+        }
+
+        if (args.event.type == 'mousedown') {
+            this.chart = chart
+            this.args = args
+        }
+    }
+    afterEvent(chart, args, options) {
+        // console.log('[afterEvent]', args.event.type)
+    }
+    afterInit(chart, args) {
+        // Add event listener to the canvas element
+        chart.canvas.addEventListener('dblclick', (event) => this.handlDoubleClick(event));
+    }
+    handlDoubleClick(event) {
+        // Get the clicked point
+        if (this.chart) {
+            const chart = this.chart
+            const activePoints = chart.getElementsAtEventForMode(event, 'point', chart.options);
+            console.log('[activePoints]', activePoints, this.chart)
+        }
+
+        // Check if there is at least one active point
+        if (activePoints.length > 0) {
+            // Get the first active point
+            // var firstPoint = activePoints[0];
+
+            // // Perform custom actions here
+            // // You can access the dataset and index of the clicked point
+            // var datasetIndex = firstPoint.datasetIndex;
+            // var index = firstPoint.index;
+
+            // Example: Log the dataset and index of the clicked point
+            console.log('Dataset Index:', datasetIndex);
+            console.log('Point Index:', index);
+        } else {
+
+        }
+        event.preventDefault()
+    }
+    drawOnExternalTooltip(context) {
+        // Tooltip Element
+        let tooltipEl = document.getElementById('chartjs-tooltip');
+
+        // Create element on first render
+        if (!tooltipEl) {
+            tooltipEl = document.createElement('div');
+            tooltipEl.id = 'chartjs-tooltip';
+            tooltipEl.innerHTML = '<div class="test">This is test</div>';
+            document.body.appendChild(tooltipEl);
+        }
+
+        // Hide if no tooltip
+        const tooltipModel = context.tooltip;
+        if (tooltipModel.opacity === 0) {
+            tooltipEl.style.opacity = 0;
             return;
         }
+
+        // Set caret Position
+        tooltipEl.classList.remove('above', 'below', 'no-transform');
+        if (tooltipModel.yAlign) {
+            tooltipEl.classList.add(tooltipModel.yAlign);
+        } else {
+            tooltipEl.classList.add('no-transform');
+        }
+
+        function getBody(bodyItem) {
+            return bodyItem.lines;
+        }
+
+        // Set Text
+        if (tooltipModel.body) {
+            const titleLines = tooltipModel.title || [];
+            const bodyLines = tooltipModel.body.map(getBody);
+
+            let innerHtml = '<thead>';
+
+            titleLines.forEach(function (title) {
+                innerHtml += '<tr><th>' + title + '</th></tr>';
+            });
+            innerHtml += '</thead><tbody>';
+
+            bodyLines.forEach(function (body, i) {
+                const colors = tooltipModel.labelColors[i];
+                let style = 'background:' + colors.backgroundColor;
+                style += '; border-color:' + colors.borderColor;
+                style += '; border-width: 2px';
+                const span = '<span style="' + style + '">' + body + '</span>';
+                innerHtml += '<tr><td>' + span + '</td></tr>';
+            });
+            innerHtml += '</tbody>';
+
+            let tableRoot = tooltipEl.querySelector('table');
+            tableRoot.innerHTML = innerHtml;
+        }
+
+        const position = context.chart.canvas.getBoundingClientRect();
+        const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
+
+        // Display, position, and set styles for font
+        tooltipEl.style.opacity = 1;
+        tooltipEl.style.position = 'absolute';
+        tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+        tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+        tooltipEl.style.font = bodyFont.string;
+        tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
+        tooltipEl.style.pointerEvents = 'none';
     }
 
 
@@ -39,9 +145,12 @@ export class AnnotationDragger {
                 case "mousedown":
                     this.lastEvent = event;
                     break;
-                default:
+                default: {
+                    console.log('[handleDrag]', event)
+                }
             }
         }
+
         return true
     }
 
